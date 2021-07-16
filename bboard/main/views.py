812a68fc0474c90f812f517a19eaf20c71c39532
IlyaNesterov <Eildos95@gmail.com>
2021-07-16 12:23:28 +0300

    create other_page view

--- bboard/main/views.py
+++ bboard/main/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
+
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def other_page(request, page):
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
