bcd2bc08a14d7221275bf6f8cca36f16168637be
IlyaNesterov <Eildos95@gmail.com>
2021-07-16 13:06:58 +0300

    create `BBLogoutView`: (url path, template)

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, other_page, BBLoginView, profile
+from .views import *
 
 app_name = 'main'
 
 urlpatterns = [
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def index(request):
@@ -23,8 +24,10 @@ class BBLoginView(LoginView):
     template_name = 'main/login.html'
 
 
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
+
+
 @login_required
 def profile(request):
     return render(request, 'main/profile.html')
-
-
