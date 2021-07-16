22b0e1a3e1cd2621027255d4f061817d67adb605
IlyaNesterov <Eildos95@gmail.com>
2021-07-16 15:34:56 +0300

    create `DeleteUserView`: (url path, template)

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('accounts/register/activate/<str:sign>/', user_activate, name='register_activate'),
     path('accounts/register/done', RegisterDoneView.as_view(), name='register_done'),
     path('accounts/register', RegisterUserView.as_view(), name='register'),
+    path('accounts/profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
     path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
     path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
