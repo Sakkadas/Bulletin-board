4f08bd59ad8c1b99ceb785773951bfe4797837e0
IlyaNesterov <Eildos95@gmail.com>
2021-07-17 06:08:45 +0300

    create `SubRubricForm`

--- bboard/main/forms.py
+++ bboard/main/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import password_validation
 from django.core.exceptions import ValidationError
 from .apps import user_registered
 
-from .models import AdvUser
+from .models import AdvUser, SubRubric, SuperRubric
 
 
 class ChangeUserInfoForm(forms.ModelForm):
@@ -52,3 +52,13 @@ class RegisterUserForm(forms.ModelForm):
         model = AdvUser
         fields = ('username', 'email', 'password1', 'password2',
                   'first_name', 'last_name', 'send_messages')
+
+
+class SubRubricForm(forms.ModelForm):
+    super_rubric = forms.ModelChoiceField(queryset=SuperRubric.objects.all(),
+                                          empty_label=None, label='Надрубрика',
+                                          required=True)
+
+    class Meta:
+        model = SubRubric
+        fields = '__all__'
