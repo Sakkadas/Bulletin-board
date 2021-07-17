72014008bb6d4ff0e19714e8940549571fb2411c
IlyaNesterov <Eildos95@gmail.com>
2021-07-17 05:20:26 +0300

    create `Rubric`,`SuperRubric` models

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -8,6 +8,33 @@ class AdvUser(AbstractUser):
     send_messages = models.BooleanField(default=True,
                                         verbose_name='Слать оповещения о новых комментариях?')
 
-
     class Meta(AbstractUser.Meta):
         pass
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, unique=True,
+                            verbose_name='Название')
+    order = models.SmallIntegerField(default=0, db_index=True,
+                                     verbose_name='Порядок')
+    super_rubric = models.ForeignKey('SuperRubric', on_delete=models.PROTECT,
+                                     null=True, blank=True, verbose_name='Надрубрика')
+
+
+
+class SuperRubricManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_rubric__isnull=True)
+
+class SuperRubric(Rubric):
+    objects = SuperRubricManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        proxy = True
+        ordering = ('order', 'name')
+        verbose_name = 'Надрубрика'
+        verbose_name_plural = 'Надрубрики'
+
