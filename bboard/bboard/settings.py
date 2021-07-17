50abb132ce23b33351d4457209298a653e77b43d
IlyaNesterov <Eildos95@gmail.com>
2021-07-17 06:21:01 +0300

    create context_processor for rubrics

--- bboard/bboard/settings.py
+++ bboard/bboard/settings.py
@@ -48,6 +48,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'main.middlewares.bboard_context_processor',
             ],
         },
     },
---
+++ bboard/main/middlewares.py
@@ -0,0 +1,8 @@
+from .models import SubRubric
+
+
+def bboard_context_processor(request):
+    context = {}
+    context['rubrics'] = SubRubric.objects.all()
+    return context
+
