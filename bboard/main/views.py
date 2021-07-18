a43093675aac8dc5ff18620da9b0f7d9fe1c924c
IlyaNesterov <Eildos95@gmail.com>
2021-07-18 12:49:43 +0300

    add comment forms for anon and auth users

--- bboard/main/views.py
+++ bboard/main/views.py
@@ -15,8 +15,8 @@ from django.views.generic.base import TemplateView
 
 from django.core.paginator import Paginator
 from django.db.models import Q
-from .models import AdvUser, SubRubric, Bb
-from .forms import ChangeUserInfoForm, RegisterUserForm, SearchForm, BbForm, AIFormSet
+
+from .forms import *
 from .utilities import signer
 
 
@@ -202,7 +202,25 @@ def by_rubrics(request, pk):
 
 
 def detail(request, rubric_pk, pk):
-    bb = get_object_or_404(Bb, pk=pk)
-    ais = bb.aditionalimage_set.all()
-    context = {'bb': bb, 'ais': ais}
-    return render(request, 'main/by_rubric.html', context)
+    bb = Bb.objects.get(pk=pk)
+    ais = bb.additionalimage_set.all()
+    comments = Comment.objects.filter(bb=pk, is_active=True)
+    initial = {'bb': bb.pk}
+    if request.user.is_authenticated:
+        initial['author'] = request.user.username
+        form_class = UserCommentForm
+    else:
+        form_class = GuestCommentForm
+    form = form_class(initial=initial)
+    if request.method == 'POST':
+        c_form = form_class(request.POST)
+        if c_form.is_valid():
+            c_form.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Комментарий добавлен')
+        else:
+            form = c_form
+            messages.add_message(request, messages.WARNING,
+                                 'Комментарий не добавлен')
+    context = {'bb': bb, 'ais': ais, 'comments': comments, 'form': form}
+    return render(request, 'main/detail.html', context)
