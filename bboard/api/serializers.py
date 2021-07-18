3c5e313e6818093c58b402aecb20f438b7df6c30
IlyaNesterov <Eildos95@gmail.com>
2021-07-18 13:30:56 +0300

    create `BbSerializer` for `Bb` model

---
+++ bboard/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from main.models import Bb
+
+
+class BbSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bb
+        fields = ('id', 'title', 'content', 'price', 'created_at')
