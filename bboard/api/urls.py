7f4ff90d2c8503311fd09f002a7932a38cf0b2be
IlyaNesterov <Eildos95@gmail.com>
2021-07-18 13:58:32 +0300

    create `BbDetailView` for `BbDetailSerializer` serializer, url path

--- bboard/api/urls.py
+++ bboard/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import bbs
+from .views import bbs, BbDetailView
 
 urlpatterns = [
+    path('bbs/<int:pk>/', BbDetailView.as_view()),
     path('bbs/', bbs),
 ]
--- bboard/api/views.py
+++ bboard/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import RetrieveAPIView
 
 from main.models import Bb
-from .serializers import BbSerializer
+from .serializers import BbSerializer, BbDetailSerializer
 
 
 @api_view(['GET'])
@@ -12,3 +13,7 @@ def bbs(request):
         bbs = Bb.objects.filter(is_active=True)[:10]
         serializer = BbSerializer(bbs, many=True)
         return Response(serializer.data)
+
+class BbDetailView(RetrieveAPIView):
+    queryset = Bb.objects.filter(is_active=True)
+    serializer_class = BbDetailSerializer
