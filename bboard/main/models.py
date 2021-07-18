127332c4416d09d0e224a8ddb9e7ddfb16e470f2
IlyaNesterov <Eildos95@gmail.com>
2021-07-18 08:13:31 +0300

    create `BB`, `AdditionalImage` models for ads, make timestamp for img

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from .utilities import get_timestamp_path
 
 class AdvUser(AbstractUser):
     is_activated = models.BooleanField(default=True, db_index=True,
@@ -56,3 +57,41 @@ class SubRubric(Rubric):
                     'name')
         verbose_name = 'Подрубрика'
         verbose_name_plural = 'Подрубрики'
+
+
+class Bb(models.Model):
+    rubric = models.ForeignKey(SubRubric, on_delete=models.PROTECT,
+                               verbose_name='Рубрика')
+    title = models.CharField(max_length=40, verbose_name='Товар')
+    content = models.TextField(verbose_name='Описание')
+    price = models.FloatField(default=0, verbose_name='Цена')
+    contacts = models.TextField(verbose_name='Контакты')
+    image = models.ImageField(blank=True, upload_to=get_timestamp_path,
+                              verbose_name='Изображение')
+    author = models.ForeignKey(AdvUser, on_delete=models.CASCADE,
+                               verbose_name='Автор объявления')
+    is_active = models.BooleanField(default=True, db_index=True,
+                                    verbose_name='Выводить в списке?')
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True,
+                                      verbose_name='Опубликовано')
+
+    def delete(self, *args, **kwargs):
+        for ai in self.additionalimage_set.all():
+            ai.delete()
+        super().delete(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-created_at']
+
+
+class AdditionalImage(models.Model):
+    bb = models.ForeignKey(Bb, on_delete=models.CASCADE,
+                           verbose_name='Объявление')
+    image = models.ImageField(upload_to=get_timestamp_path,
+                              verbose_name='Изображение')
+
+    class Meta:
+        verbose_name_plural = 'Дополнительные иллюстрации'
+        verbose_name = 'Дополнительная иллюстрация'
--- bboard/main/utilities.py
+++ bboard/main/utilities.py
@@ -1,10 +1,12 @@
 from django.template.loader import render_to_string
 from django.core.signing import Signer
-
+from datetime import datetime
+from os.path import splitext
 
 from bboard.settings import ALLOWED_HOSTS
 
 signer = Signer()
+
 
 def send_activation_notification(user):
     if ALLOWED_HOSTS:
@@ -15,3 +17,7 @@ def send_activation_notification(user):
     subject = render_to_string('email/activation_letter_subject.txt', context)
     body_text = render_to_string('email/activation_letter_body.txt', context)
     user.email_user(subject, body_text)
+
+
+def get_timestamp_path(instance, filename):
+    return '%s%s' % (datetime.now().timestamp(), splitext(filename)[1])
