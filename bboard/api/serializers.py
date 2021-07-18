46144236894000014d293eedc9d6db6d8e16d8d5
IlyaNesterov <Eildos95@gmail.com>
2021-07-18 14:02:25 +0300

    create `CommentSerializer`, view, url path

--- bboard/api/serializers.py
+++ bboard/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from main.models import Bb
+from main.models import Bb, Comment
 
 
 class BbSerializer(serializers.ModelSerializer):
@@ -8,8 +8,15 @@ class BbSerializer(serializers.ModelSerializer):
         model = Bb
         fields = ('id', 'title', 'content', 'price', 'created_at')
 
+
 class BbDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bb
         fields = ('id', 'title', 'content', 'price', 'created_at',
                   'contacts', 'image')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('bb', 'author', 'content', 'created_at')
