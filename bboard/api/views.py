a2a02a48569bb34f6ab001b04071db72e1626435
IlyaNesterov <Eildos95@gmail.com>
2021-07-18 13:31:31 +0300

    create bbs view, and path

--- bboard/api/views.py
+++ bboard/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from main.models import Bb
+from .serializers import BbSerializer
+
+
+@api_view(['GET'])
+def bbs(request):
+    if request.method == 'GET':
+        bbs = Bb.objects.filter(is_active=True)[:10]
+        serializer = BbSerializer(bbs, many=True)
+        return Response(serializer.data)
