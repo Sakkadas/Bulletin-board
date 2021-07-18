a06f0b9584d675276d619720069d669698600ab3
IlyaNesterov <Eildos95@gmail.com>
2021-07-18 13:57:55 +0300

    create `BbDetailSerializer`

--- bboard/api/serializers.py
+++ bboard/api/serializers.py
@@ -7,3 +7,9 @@ class BbSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bb
         fields = ('id', 'title', 'content', 'price', 'created_at')
+
+class BbDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bb
+        fields = ('id', 'title', 'content', 'price', 'created_at',
+                  'contacts', 'image')
