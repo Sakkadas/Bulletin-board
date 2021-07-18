fec0d7e2f0b12442c10367b44e7fac6d4ae0ad96
IlyaNesterov <Eildos95@gmail.com>
2021-07-18 08:16:51 +0300

    add delete method for users to `AdvUser`

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -3,11 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 from .utilities import get_timestamp_path
 
+
 class AdvUser(AbstractUser):
     is_activated = models.BooleanField(default=True, db_index=True,
                                        verbose_name='Прошел активацию?')
     send_messages = models.BooleanField(default=True,
                                         verbose_name='Слать оповещения о новых комментариях?')
+
+    def delete(self, *args, **kwargs):
+        for bb in self.bb_set.all():
+            bb.delete()
+        super().delete(*args, **kwargs)
 
     class Meta(AbstractUser.Meta):
         pass
