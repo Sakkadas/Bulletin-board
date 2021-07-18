dd467e5980941495d6e4eb67ca5391fad7bb8f7a
IlyaNesterov <Eildos95@gmail.com>
2021-07-18 13:32:39 +0300

    join api app with main urls

--- bboard/bboard/urls.py
+++ bboard/bboard/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('captcha/', include('captcha.urls')),
+    path('api/', include('api.urls')),
     path('', include("main.urls")),
 ]
 
