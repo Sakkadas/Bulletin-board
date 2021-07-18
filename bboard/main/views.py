62d2557136204ace51867a6e7a2a40eea9ebd68d
IlyaNesterov <Eildos95@gmail.com>
2021-07-18 12:14:50 +0300

    create `profile_bb_change`, `profile_bb_delete` views, url paths

--- bboard/main/views.py
+++ bboard/main/views.py
@@ -130,6 +130,38 @@ def profile_bb_add(request):
     return render(request, 'main/profile_bb_add.html', context)
 
 
+@login_required
+def profile_bb_change(request, pk):
+    bb = get_object_or_404(Bb, pk=pk)
+    if request.method == 'POST':
+        form = BbForm(request.POST, request.FILES, instance=bb)
+        if form.is_valid():
+            bb = form.save()
+            formset = AIFormSet(request.POST, request.FILES, instance=bb)
+            if formset.is_valid():
+                formset.save()
+                messages.add_message(request, messages.SUCCESS,
+                                     'Объявление исправлено')
+                return redirect('main:profile')
+    else:
+        form = BbForm(instance=bb)
+        formset = AIFormSet(instance=bb)
+    context = {'form': form, 'formset': formset}
+    return render(request, 'main/profile_bb_change.html', context)
+
+
+@login_required
+def profile_bb_delete(request, pk):
+    bb = get_object_or_404(Bb, pk=pk)
+    if request.method == 'POST':
+        bb.delete()
+        messages.add_message(request, messages.SUCCESS, 'Объявление удалено')
+        return redirect('main:profile')
+    else:
+        context = {'bb': bb}
+        return render(request, 'main/profile_bb_delete.html', context)
+
+
 class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin,
                          UpdateView):
     model = AdvUser
